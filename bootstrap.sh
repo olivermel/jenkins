e69f2af06c527d443e566e774a204fad9d7645ba
olivermel <attybee10@yahoo.com>
2016-08-29 12:09:30 -0400

    jenkins

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,10 @@ sudo yum -y groupinstall 'Development Tools'
 sudo yum -y install vim tree 
 sudo yum -y install libicu-devel
 
-
-
+cd /tmp
+sudo wget -O /etc/yum.repos.d/jenkins.repo  http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo yum -y install jenkins 
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo service jenkins start 
+sudo chkconfig jenkins on
+sudo yum update -y
